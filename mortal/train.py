3a3ce9361d2128e3364246e371caa37a5c82c8e4
lichao <lichao@hz.com>
2026-08-08 15:09:01 +0800

    optimze train.py without online conditions

--- mortal/train.py
+++ mortal/train.py
@@ -2,8 +2,6 @@ def train():
     import prelude
 
     import logging
-    import sys
-    import os
     import gc
     import gzip
     import json
@@ -19,7 +17,7 @@ def train():
     from torch.nn.utils import clip_grad_norm_
     from torch.utils.data import DataLoader
     from torch.utils.tensorboard import SummaryWriter
-    from common import submit_param, parameter_count, drain, filtered_trimmed_lines, tqdm
+    from common import parameter_count, filtered_trimmed_lines, tqdm
     from player import TestPlayer
     from dataloader import FileDatasetsIter, worker_init_fn
     from lr_scheduler import LinearWarmUpCosineAnnealingLR
@@ -29,12 +27,10 @@ def train():
 
     version = config['control']['version']
 
-    online = config['control']['online']
     batch_size = config['control']['batch_size']
     opt_step_every = config['control']['opt_step_every']
     save_every = config['control']['save_every']
     test_every = config['control']['test_every']
-    submit_every = config['control']['submit_every']
     test_games = config['test_play']['games']
     min_q_weight = config['cql']['min_q_weight']
     next_rank_weight = config['aux']['next_rank_weight']
@@ -110,9 +106,8 @@ def train():
         mortal.load_state_dict(state['mortal'])
         dqn.load_state_dict(state['current_dqn'])
         aux_net.load_state_dict(state['aux_net'])
-        if not online or state['config']['control']['online']:
-            optimizer.load_state_dict(state['optimizer'])
-            scheduler.load_state_dict(state['scheduler'])
+        optimizer.load_state_dict(state['optimizer'])
+        scheduler.load_state_dict(state['scheduler'])
         scaler.load_state_dict(state['scaler'])
         best_perf = state['best_perf']
         steps = state['steps']
@@ -125,10 +120,6 @@ def train():
         logging.info(f'device: {device} ({torch.cuda.get_device_name(device)})')
     else:
         logging.info(f'device: {device}')
-
-    if online:
-        submit_param(mortal, dqn, is_idle=True)
-        logging.info('param has been submitted')
 
     writer = SummaryWriter(config['control']['tensorboard_dir'])
     stats = {
@@ -144,38 +135,32 @@ def train():
         nonlocal steps
         nonlocal idx
 
-        player_names = []
-        if online:
-            player_names = ['trainee']
-            dirname = drain()
-            file_list = list(map(lambda p: path.join(dirname, p), os.listdir(dirname)))
-        else:
-            player_names_set = set()
-            for filename in config['dataset']['player_names_files']:
-                with open(filename) as f:
-                    player_names_set.update(filtered_trimmed_lines(f))
-            player_names = list(player_names_set)
-            logging.info(f'loaded {len(player_names):,} players')
+        player_names_set = set()
+        for filename in config['dataset']['player_names_files']:
+            with open(filename) as f:
+                player_names_set.update(filtered_trimmed_lines(f))
+        player_names = list(player_names_set)
+        logging.info(f'loaded {len(player_names):,} players')
 
-            file_index = config['dataset']['file_index']
-            if path.exists(file_index):
-                index = torch.load(file_index, weights_only=True)
-                file_list = index['file_list']
-            else:
-                logging.info('building file index...')
-                file_list = []
-                for pat in config['dataset']['globs']:
-                    file_list.extend(glob(pat, recursive=True))
-                if len(player_names_set) > 0:
-                    filtered = []
-                    for filename in tqdm(file_list, unit='file'):
-                        with gzip.open(filename, 'rt') as f:
-                            start = json.loads(next(f))
-                            if not set(start['names']).isdisjoint(player_names_set):
-                                filtered.append(filename)
-                    file_list = filtered
-                file_list.sort(reverse=True)
-                torch.save({'file_list': file_list}, file_index)
+        file_index = config['dataset']['file_index']
+        if path.exists(file_index):
+            index = torch.load(file_index, weights_only=True)
+            file_list = index['file_list']
+        else:
+            logging.info('building file index...')
+            file_list = []
+            for pat in config['dataset']['globs']:
+                file_list.extend(glob(pat, recursive=True))
+            if len(player_names_set) > 0:
+                filtered = []
+                for filename in tqdm(file_list, unit='file'):
+                    with gzip.open(filename, 'rt') as f:
+                        start = json.loads(next(f))
+                        if not set(start['names']).isdisjoint(player_names_set):
+                            filtered.append(filename)
+                file_list = filtered
+            file_list.sort(reverse=True)
+            torch.save({'file_list': file_list}, file_index)
         logging.info(f'file list size: {len(file_list):,}')
 
         before_next_test_play = (test_every - steps % test_every) % test_every
@@ -233,9 +218,7 @@ def train():
                 q_out = dqn(phi, masks)
                 q = q_out[range(batch_size), actions]
                 dqn_loss = 0.5 * mse(q, q_target_mc)
-                cql_loss = 0
-                if not online:
-                    cql_loss = q_out.logsumexp(-1).mean() - q.mean()
+                cql_loss = q_out.logsumexp(-1).mean() - q.mean()
 
                 next_rank_logits, = aux_net(phi)
                 next_rank_loss = ce(next_rank_logits, player_ranks)
@@ -249,8 +232,7 @@ def train():
 
             with torch.inference_mode():
                 stats['dqn_loss'] += dqn_loss
-                if not online:
-                    stats['cql_loss'] += cql_loss
+                stats['cql_loss'] += cql_loss
                 stats['next_rank_loss'] += next_rank_loss
                 all_q[idx] = q
                 all_q_target[idx] = q_target_mc
@@ -268,10 +250,6 @@ def train():
             scheduler.step()
             pb.update(1)
 
-            if online and steps % submit_every == 0:
-                submit_param(mortal, dqn, is_idle=False)
-                logging.info('param has been submitted')
-
             if steps % save_every == 0:
                 pb.close()
 
@@ -280,8 +258,7 @@ def train():
                 all_q_target_1d = all_q_target.cpu().numpy().flatten()[::128]
 
                 writer.add_scalar('loss/dqn_loss', stats['dqn_loss'] / save_every, steps)
-                if not online:
-                    writer.add_scalar('loss/cql_loss', stats['cql_loss'] / save_every, steps)
+                writer.add_scalar('loss/cql_loss', stats['cql_loss'] / save_every, steps)
                 writer.add_scalar('loss/next_rank_loss', stats['next_rank_loss'] / save_every, steps)
                 writer.add_scalar('hparam/lr', scheduler.get_last_lr()[0], steps)
                 writer.add_histogram('q_predicted', all_q_1d, steps)
@@ -308,10 +285,6 @@ def train():
                     'config': config,
                 }
                 torch.save(state, state_file)
-
-                if online and steps % submit_every != 0:
-                    submit_param(mortal, dqn, is_idle=False)
-                    logging.info('param has been submitted')
 
                 if steps % test_every == 0:
                     stat = test_player.test_play(test_games // 4, mortal, dqn, device)
@@ -378,12 +351,6 @@ def train():
                             f'saving to {best_state_file}'
                         )
                         shutil.copy(state_file, best_state_file)
-                    if online:
-                        # BUG: This is a bug with unknown reason. When training
-                        # in online mode, the process will get stuck here. This
-                        # is the reason why `main` spawns a sub process to train
-                        # in online mode instead of going for training directly.
-                        sys.exit(0)
                 pb = tqdm(total=save_every, desc='TRAIN')
 
         for obs, actions, masks, steps_to_done, kyoku_rewards, player_ranks in data_loader:
@@ -422,49 +389,14 @@ def train():
                 end += batch_size
         pb.close()
 
-        if online:
-            submit_param(mortal, dqn, is_idle=True)
-            logging.info('param has been submitted')
-
-    while True:
-        train_epoch()
-        gc.collect()
-        # torch.cuda.empty_cache()
-        # torch.cuda.synchronize()
-        if not online:
-            # only run one epoch for offline for easier control
-            break
+    # only run one epoch for offline for easier control
+    train_epoch()
+    gc.collect()
+    # torch.cuda.empty_cache()
+    # torch.cuda.synchronize()
 
 def main():
-    import os
-    import sys
-    import time
-    from subprocess import Popen
-    from config import config
-
-    # do not set this env manually
-    is_sub_proc_key = 'MORTAL_IS_SUB_PROC'
-    online = config['control']['online']
-    if not online or os.environ.get(is_sub_proc_key, '0') == '1':
-        train()
-        return
-
-    cmd = (sys.executable, __file__)
-    env = {
-        is_sub_proc_key: '1',
-        **os.environ.copy(),
-    }
-    while True:
-        child = Popen(
-            cmd,
-            stdin = sys.stdin,
-            stdout = sys.stdout,
-            stderr = sys.stderr,
-            env = env,
-        )
-        if (code := child.wait()) != 0:
-            sys.exit(code)
-        time.sleep(3)
+    train()
 
 if __name__ == '__main__':
     try:
