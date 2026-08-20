216b64488905846a1c86e91cc980e689078f6c85
lichao <lichao@hz.com>
2026-08-20 10:08:35 +0800

    save

--- mortal/train.py
+++ mortal/train.py
@@ -322,7 +322,7 @@ def train():
         logging.info(f'training done, saved at step {steps:,}')
 
     # train multiple epochs on the preloaded dataset (loaded once above)
-    for i in range(1000):
+    for i in range(4):
         train_epoch()
         gc.collect()
     # torch.cuda.empty_cache()
